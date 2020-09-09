2e79dae98947601ee8ae5393979d9ea439b6d44c
Randy Douglas <randy.e.douglas@gmail.com>
2020-09-09 10:52:49 -0400

    added fetch

--- src/BookTitle/BookTitle.js
+++ src/BookTitle/BookTitle.js
@@ -2,16 +2,15 @@ import React from 'react';
 
 function BookTitle(props){
   console.log(props.books)
-for (let i = 0; i < props.books.length; i++){
-  console.log(props.books[i].volumeInfo.title)
-  let titles = [];
-  let title = props.books[i].volumeInfo.title
-  title.push(title)
-  return titles
-}
-return null
-   // {bookTitles}
-  
+  const bookTitles = props.books.map((book) => {
+    console.log(book)
+        <h2 key={ind}>{title}</h2>
+
+  })
+
+  return(
+    {bookTitles}
+  )
 }
 
 export default  BookTitle
